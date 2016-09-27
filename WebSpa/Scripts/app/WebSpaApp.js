b0b5254c8920c5752a0a8e844bc584cb7d079d17
Smo0thCriminal <halfing@mail.ru>
2016-09-27 19:50:06 +0300

    commin

--- WebSpa/Scripts/app/WebSpaApp.js
+++ WebSpa/Scripts/app/WebSpaApp.js
@@ -1,41 +1,31 @@
-﻿(function() {
-    'use strict';
-
-    angular
-        .module('WebSpaApp')
-        .config(config);
-
-    config.$inject = ['$stateProvider'];
-
-    function config($stateProvider) {
-        $stateProvider
-            .state('home',
-            {
-                url: '/',
-                templateUrl: '/Scripts/app/template/Introduce.html',
-                controller: 'LoginController'
-            });
-    }
-})();
-
-//var angular = angular.module('WebSpaApp', ['ngRoute'])
-//    .config(function ($routeProvider) {
-//        $routeProvider
-//            .when('/',
-//            {
-//                templateUrl: '/Scripts/app/template/Introduce.html',
-//                controller: 'LoginController'
-//            })
-
-//            .when('/ManageQuiz',
-//            {
-//             templateUrl: '/Scripts/app/template/ManageData.html',
-//             controller: 'ManageController'
-//            })
-//            .when('/Quiz', 
-//            {
-//                templateUrl: '/Scripts/app/template/Quiz.html',
-//                controller: 'QuizController'
-//            })
-//            .otherwise({ redirectTo: '/' });
-//    });
+﻿var angular = angular.module('WebSpaApp', ['ui.router']).config($stateProvider)
+    $stateProvider
+        .state('Introduce', {
+            url: '/Introduce',
+            templateUrl: '/Scripts/app/template/Introduce.html',
+            controller: 'LoginController as vm'
+        });
+        //.state('ManageQuiz', {
+        //    url: '/ManageQuiz',
+        //    templateUrl: '/Scripts/app/template/ManageData.html',
+        //    controller: 'ManageController as vm'
+        //});
+}
+    //.config(function ($routeProvider) {
+    //    $routeProvider
+    //        .when('/',
+    //        {
+    //            templateUrl: '/Scripts/app/template/Introduce.html',
+    //            controller: 'LoginController'
+    //        })
+    //        .when('/ManageQuiz',
+    //        {
+    //         templateUrl: '/Scripts/app/template/ManageData.html',
+    //         controller: 'ManageController'
+    //        })
+    //        .when('/Quiz', 
+    //        {
+    //            templateUrl: '/Scripts/app/template/Quiz.html',
+    //            controller: 'QuizController'
+    //        })
+    //        .otherwise({ redirectTo: '/' });
